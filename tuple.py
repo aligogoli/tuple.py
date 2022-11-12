9dc05a015d5209f7767acb1825827d008beee44a
aliquazaimodo <ajali4487@gmail.com>
2022-11-12 23:08:20 +0330

    Create tuple.py
    this is my tuple project and i I got acquainted with this concept

---
+++ tuple.py
@@ -0,0 +1,7 @@
+
+t = tuple()
+ghad ={'amir':184,'hossein':176,'hamed':198,'milad':200}
+print(ghad.items())
+list(ghad.items())
+for name,ghad in list(ghad.items()):
+    print('ghad hast:',(name,ghad))
